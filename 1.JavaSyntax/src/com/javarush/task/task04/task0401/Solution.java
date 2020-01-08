98ef1a68d3c54ed044f334f1dd48cfb18df73bc9
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-08 19:47:02 +0300

    add solution to task0401

--- 1.JavaSyntax/src/com/javarush/task/task04/task0401/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0401/Solution.java
@@ -3,13 +3,13 @@ package com.javarush.task.task04.task0401;
 /* 
 Мне не подходит этот возраст…
 */
+
 public class Solution {
     public static void main(String[] args) {
-
         Person person = new Person();
         System.out.println("Age is: " + person.age);
         person.adjustAge(person.age);
-        System.out.println("Adjusted Age is: " + person.age);
+        System.out.println("Adjusted age is: " + person.age);
     }
 
     public static class Person {
@@ -17,8 +17,7 @@ public class Solution {
 
         public void adjustAge(int age) {
             this.age = age + 20;
-
-            System.out.println("The Age in adjustAge() is " + age);
+            System.out.println("Age in adjustAge() is: " + age);
         }
     }
 }
