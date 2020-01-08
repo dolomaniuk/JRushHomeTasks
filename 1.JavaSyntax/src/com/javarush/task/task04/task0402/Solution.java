fcf078c72d44a6ee7d2b3f2e34e08966234e54a4
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-08 20:34:46 +0300

    add solution to task0402

--- 1.JavaSyntax/src/com/javarush/task/task04/task0402/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0402/Solution.java
@@ -1,15 +1,16 @@
 package com.javarush.task.task04.task0402;
 
 /* 
-Цена яблока
+Цена яблок
 */
+
 public class Solution {
     public static void main(String[] args) {
         Apple apple = new Apple();
         apple.addPrice(50);
         Apple apple2 = new Apple();
         apple2.addPrice(100);
-        System.out.println("Apples price is " + Apple.applesPrice);
+        System.out.println("Стоимость яблок " + Apple.applesPrice);
     }
 
     public static class Apple {
@@ -17,7 +18,7 @@ public class Solution {
 
         public static void addPrice(int applesPrice) {
             //напишите тут ваш код
-            Apple.applesPrice +=applesPrice;
+            Apple.applesPrice += applesPrice;
         }
     }
 }
