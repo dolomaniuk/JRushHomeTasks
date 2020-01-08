46242492df8b9679b2ba121a4bd117fbab1ecc16
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-08 20:41:47 +0300

    update solution to task0403

--- 1.JavaSyntax/src/com/javarush/task/task04/task0403/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0403/Cat.java
@@ -1,18 +1,21 @@
 package com.javarush.task.task04.task0403;
 
 /* 
-Реализовать метод setName
+Как зовут кота?
 */
 
 public class Cat {
-    private String name;
+    private String name = "безымянный кот";
 
     public void setName(String name) {
         //напишите тут ваш код
         this.name = name;
+
     }
 
     public static void main(String[] args) {
-
+        Cat cat = new Cat();
+        cat.setName("Жужик");
+        System.out.println(cat.name);
     }
 }
