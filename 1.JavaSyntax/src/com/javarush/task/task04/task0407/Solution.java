fc8dd31fae6a090f35c3e227a050511d85192cbc
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-08 22:08:59 +0300

    update solution to task0407

--- 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0407/Solution.java
@@ -7,21 +7,15 @@ package com.javarush.task.task04.task0407;
 public class Solution {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
-        Cat.countCat();
-        //напишите тут ваш код
+        Cat.count ++;
 
         Cat cat2 = new Cat();
-        Cat.countCat();
-        //напишите тут ваш код
+        Cat.count ++;
 
-        System.out.println("Cats count is " + Cat.count);
+        System.out.println("The cat count is " + Cat.count);
     }
 
     public static class Cat {
         public static int count = 0;
-
-public static void countCat(){
-    Cat.count++;
-}
     }
 }
