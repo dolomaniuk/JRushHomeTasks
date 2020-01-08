f627096682490bf145d199e454a243d8e9187b38
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-08 22:00:46 +0300

    update solution to task0405

--- 1.JavaSyntax/src/com/javarush/task/task04/task0405/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0405/Cat.java
@@ -1,18 +1,19 @@
 package com.javarush.task.task04.task0405;
 
 /* 
-Реализовать метод setCatsCount
+Задаем количество котов
 */
 
 public class Cat {
     private static int catsCount = 0;
 
     public static void setCatsCount(int catsCount) {
-        //напишите тут ваш код
         Cat.catsCount = catsCount;
     }
 
     public static void main(String[] args) {
-setCatsCount(5);
+        System.out.println(catsCount);
+        setCatsCount(4);
+        System.out.println(catsCount);
     }
 }
