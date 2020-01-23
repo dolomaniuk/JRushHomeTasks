29d9de10c4eab34bf3b617bbe082498a05cf9b70
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-24 00:01:27 +0300

    add solution to task0420 // sorting 3 numbers

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task04.task0420;
+
+/* 
+Сортировка трех чисел
+*/
+
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(buf.readLine());
+        int b = Integer.parseInt(buf.readLine());
+        int c = Integer.parseInt(buf.readLine());
+
+        int first_number = max(max(a, b), max(b, c));
+        int third_number = min(min(a, b), min(b, c));
+        int second_number = 0;
+
+        if (a == first_number) second_number = max(b, c);
+        else if (b == first_number) second_number = max(a,c);
+        else second_number = max(a, b);
+        System.out.printf("%d %d %d", first_number, second_number, third_number);
+    }
+}
