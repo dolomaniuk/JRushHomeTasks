154d7b21971b22f9263443fd6fb9661347a79fa6
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-25 14:54:36 +0300

    refactor task0426 with boolean

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -9,16 +9,26 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        final String ZERO = "ноль";
+        final String POZITIVE = "положительное ";
+        final String NEGATIVE = "отрицательное ";
+        final String EVEN = "четное число";
+        final String ODD = "нечетное число";
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(reader.readLine());
+
         String text = "";
-        if (number == 0) text = "ноль";
+        boolean flagZero = (number == 0);
+        boolean flagSing = (number > 0);
+        boolean flagParity = (number % 2 == 0);
+
+        if (flagZero) text = ZERO;
         else {
-            if(number > 0) text = "положительное";
-            else text = "отрицательное";
-            if(number % 2 != 0) text += " нечетное";
-            else text += " четное";
-            text += " число";
+            if(flagSing) text = POZITIVE;
+            else text = NEGATIVE;
+            if(flagParity) text += EVEN;
+            else text += ODD;
         }
         System.out.println(text);
     }
