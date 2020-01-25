4393f8c377d3967994d5614a00ab078e6fac27ee
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-26 00:52:45 +0300

    refactor task0428

--- 1.JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
@@ -10,15 +10,15 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        short count = 0;
 
-        int a = Integer.parseInt(reader.readLine());
+        short count = 0;
+        for (int i = 0; i < 3; i++) {
+            int a = Integer.parseInt(reader.readLine());
             if(a > 0) count++;
-        int b = Integer.parseInt(reader.readLine());
-            if(b > 0) count++;
-        int c = Integer.parseInt(reader.readLine());
-            if(c > 0) count++;
-            System.out.print(count);
         }
+        System.out.print(count);
+    }
+
+
     }
 
