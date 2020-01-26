da801b85dd1e68797d6b5ae01c7cd3a33725cd27
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-26 23:56:55 +0300

    add solution to task0443

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task04.task0443;
+
+/* 
+Как назвали, так назвали
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int year = Integer.parseInt(reader.readLine());
+        int month = Integer.parseInt(reader.readLine());
+        int day = Integer.parseInt(reader.readLine());
+
+        System.out.printf("Меня зовут %s.\nЯ родился %d.%d.%d", name, day, month, year);
+
+    }
+}
