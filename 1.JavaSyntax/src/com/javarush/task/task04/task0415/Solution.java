75cfdd23c15dec1564efb5b60acaef9c6ef2ac28
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-27 00:00:26 +0300

    update task0415

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -10,14 +10,11 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Input a:");
         int a = Integer.parseInt(buffer.readLine());
-        System.out.println("Input b:");
         int b = Integer.parseInt(buffer.readLine());
-        System.out.println("Input c:");
         int c = Integer.parseInt(buffer.readLine());
 
-        if(a < (b + c) && b < (a + c) && c < (a + b)) System.out.println("Треугольник существует");
-        else System.out.println("Треугольник не существует");
+        if(a < (b + c) && b < (a + c) && c < (a + b)) System.out.println("Треугольник существует.");
+        else System.out.println("Треугольник не существует.");
     }
 }
