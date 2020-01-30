3dd782429afb55446500fd28c1f6c3a8b9f12df1
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-01-31 00:06:34 +0300

    task0528 add SimpleDateFormat

--- 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -4,8 +4,14 @@ package com.javarush.task.task05.task0528;
 Вывести на экран сегодняшнюю дату
 */
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println("30 01 2020");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+        System.out.println(dateFormat.format(new Date()));
     }
 }
