02faa3f714bb1a70d58017a3113cbf651c1e9c7a
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-02-06 22:30:18 +0300

    refactor task0622

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task06.task0622;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Числа по возрастанию
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List arr = new ArrayList();
+        for(int i = 0; i < 5; i++){
+            arr.add(Integer.parseInt(reader.readLine()));
+        }
+        Collections.sort(arr);
+        for (Object counter :
+                arr) {
+            System.out.println(counter);
+        }
+    }
+}
+
