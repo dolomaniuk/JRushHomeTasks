94001652ef0719a68928842ab0b0f00b7cd713ca
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-02-08 12:52:06 +0300

    add solution to 2/18 in game 2048

---
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -0,0 +1,11 @@
+package com.javarush.games.game2048;
+
+import com.javarush.engine.cell.*;
+
+public class Game2048 extends Game{
+
+    @Override
+    public void initialize() {
+        setScreenSize(200, 200);
+    }
+}
