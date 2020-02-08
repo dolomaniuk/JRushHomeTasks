ff35e006965fef04f56ceb62e6e487c7a012fe0e
Dmitry Dolomaniuk <dalamas200487@gmail.com>
2020-02-08 13:01:08 +0300

    refactor task0701

--- 1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
@@ -12,14 +12,13 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        int[] array = initializeArray();
+        int[] array = initializeArray(new int[20]);
         int max = max(array);
         System.out.println(max);
     }
 
-    public static int[] initializeArray() throws IOException {
+    public static int[] initializeArray(int[] array) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[20];
         for (int i = 0; i < 20; i++) {
             array[i] = Integer.parseInt(reader.readLine());
         }
